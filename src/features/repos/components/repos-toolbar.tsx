0d2e8db5ece86db068e9324cae81f7dc061d934b
Marcio Sousa <marcio.sousa@ftwcomunicacao.com.br>
2026-02-01 18:57:27 -0300

    fix: adjusting the repos-tollbar for reponsive mode

--- src/features/repos/components/repos-toolbar.tsx
+++ src/features/repos/components/repos-toolbar.tsx
@@ -4,7 +4,7 @@ import { ReposFilters } from "./repos-filters";
 
 export function ReposToobar() {
   return (
-    <div className='flex justify-between gap-10'>
+    <div className='flex justify-between mt-2 gap-10 md:mt-12 xl:mt-12'>
       <div className="w-full max-w-111">
         <ReposSearchInput />
       </div>
