37ddc5525becfbe710a2ed66775bdb0cf472d011
Marcio Sousa <marcio.sousa@ftwcomunicacao.com.br>
2026-02-01 11:00:47 -0300

    feat: add filter of language and type on store filters

--- src/store/filters-store.ts
+++ src/store/filters-store.ts
@@ -1,11 +1,29 @@
 import { create } from 'zustand'
 
-type FilterStoreType = {
+type RepoTypes = "all" | 'sources' | "forks" | "archived" | "mirros" | null
+
+type FilterStoreTypes = {
   search: string
+  type: RepoTypes
+  language: string | null
   setSearch: (search: string) => void
+  setType: (value: RepoTypes) => void
+  setlanguage: (language: string | null) => void
+  resetFilter: () => void
 }
 
-export const filtersStore = create<FilterStoreType>((set, get) => ({
+export const filtersStore = create<FilterStoreTypes>((set, get) => ({
   search: "",
-  setSearch: (search: string) => set({ search })
+  type: null,
+  language: null,
+
+  setSearch: (search) => set({ search }),
+  setType: (type) => set({ type }),
+  setlanguage: (language) => set({ language }),
+
+  resetFilter: () => set({
+    search: "",
+    type: null,
+    language: null
+  })
 }))
